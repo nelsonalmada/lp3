eb7fc142c7d9095d8caf6cd7910d906d3d3f001f
nelsonalmada <almadanelson15@gmail.com>
2025-09-04 17:51:14 -0400

    Introduccion Javascrip

---
+++ javascrip/js/scrip.js
@@ -0,0 +1,4 @@
+/*Introduccipn*/
+alert("Hola mundo desde scrip.js");
+document.write("<h1>Hola mundo desde js condocument.write </h1><hr>");
+console.log("Hola esto es un mensaje consola desde scrip");
