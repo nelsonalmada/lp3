844c8266b9e4fee8ba00882a1b448f1d7e5606ae
nelsonalmada <almadanelson15@gmail.com>
2025-09-12 17:35:17 -0400

    Procesar formulario desde archivo externo

---
+++ php/enviar_con_get.php
@@ -0,0 +1,10 @@
+<h1>Formulario con GET procesado mediante otro archivo</h1>
+<html>
+    <body>
+        <form action="recibir_con_get.php" method="get">
+          Nombre: <input type="text" name="nombre">
+          Email: <input type="text" name="email">
+          <input type="submit" value="Enviar Info">
+        </form>
+    </body>
+</html>
---
+++ php/enviar_con_post.php
@@ -0,0 +1,10 @@
+<h1>Formulario con POST procesado mediante otro archivo</h1>
+<html>
+    <body>
+        <form action="recibir_con_post.php" method="post">
+          Nombre: <input type="text" name="nombre">
+          Email: <input type="text" name="email">
+          <input type="submit" value="Enviar Info">
+        </form>
+    </body>
+</html>
